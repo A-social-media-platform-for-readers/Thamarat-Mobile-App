a82b5836735430773ee2f1ff2db6299e27bf324c
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2023-12-12 13:08:28 +0200

    Add AppColors class with predefined colors

---
+++ lib/views/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color color1 = Color(0xFF92E3A9);
+  static const Color color2 = Color(0xFF48FB77);
+  static const Color color3 = Color(0xFF8B8B8B);
+}

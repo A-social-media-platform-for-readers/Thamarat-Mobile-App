f108aff575fcaddd4413cce6abeab88d67ca0315
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2023-12-12 06:03:25 +0200

    Add email input field to Login screen

--- lib/views/screens/Login_screen.dart
+++ lib/views/screens/Login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/views/common_widgets/welcome_text.dart';
+import 'package:untitled/views/utils/app_fonts.dart';
 
 import '../common_widgets/app_title.dart';
 
@@ -13,21 +14,59 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
           child: Center(
         child: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 53,
             ),
-            AppTitle(
+            const AppTitle(
               textColor: Color(0xFF92E3A9),
             ),
-            SizedBox(
+            const SizedBox(
               height: 21,
             ),
-            WelcomeText(),
+            const WelcomeText(),
+            const SizedBox(
+              height: 26,
+            ),
+            Container(
+              width: 297,
+              height: 79,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 79,
+                    child: Text(
+                      'البريد الالكتروني',
+                      textAlign: TextAlign.right,
+                      style: safeGoogleFont(
+                        'Cairo',
+                        color: const Color(0xFF8B8B8B),
+                        fontSize: 11,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsetsDirectional.only(top: 23),
+                    width: 297,
+                    height: 56,
+                    decoration: ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(
+                            width: 1, color: Color(0xFF8B8B8B)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       )),

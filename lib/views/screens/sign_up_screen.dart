398d2f9dfdf9c62c71acbc181fcf2b775b7b106c
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-01-04 22:59:21 +0200

    Add SignUpScreen

---
+++ lib/views/screens/sign_up_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const SingleChildScrollView(
+          child: Column(
+        children: [Text('Sign Up Screen')],
+      )),
+    );
+  }
+}

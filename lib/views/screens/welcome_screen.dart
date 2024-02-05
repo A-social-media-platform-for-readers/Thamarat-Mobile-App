9ae0105db3cbca6e78421472cee1061175728edb
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-02-05 22:43:54 +0200

    Add WelcomeScreen widget with UI components

---
+++ lib/views/screens/welcome_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:untitled/views/common_widgets/welcome_text.dart';
+import 'package:untitled/views/screens/login_screen.dart';
+import 'package:untitled/views/screens/Identification_screen.dart';
+import 'package:untitled/views/utils/app_colors.dart';
+import '../common_widgets/welcome_screen_button.dart';
+import '../common_widgets/app_title.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: AppColors.primary,
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 53.h,
+                  ),
+                  const AppTitle(
+                    textColor: Colors.white,
+                  ),
+                  Image.asset(
+                    'assets/reading-list-bro-1.png',
+                    width: 460.w,
+                    height: 460.h,
+                  ),
+                  const WelcomeText(),
+                  SizedBox(
+                    height: 29.h,
+                  ),
+                  const WelcomeScreenButton(
+                    lable: 'لدي حساب بالفعل',
+                    textColor: Colors.white,
+                    borderColor: Colors.white,
+                    buttonColor: AppColors.primary,
+                    screen: LoginScreen(),
+                  ),
+                  SizedBox(
+                    height: 17.h,
+                  ),
+                  const WelcomeScreenButton(
+                    lable: 'إنشاء حساب',
+                    textColor: AppColors.color2,
+                    borderColor: Colors.white,
+                    buttonColor: Colors.white,
+                    screen: IdentificationScreen(),
+                  ),
+                  SizedBox(
+                    height: 49.h,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}

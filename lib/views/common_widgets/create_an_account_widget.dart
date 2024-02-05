10285c2c1cad19941fa3e8a31f4a95b5cdfe8162
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-02-05 22:40:59 +0200

    Add CreateAnAccountWidget

---
+++ lib/views/common_widgets/create_an_account_widget.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../screens/Identification_screen.dart';
+import '../utils/app_fonts.dart';
+
+class CreateAnAccountWidget extends StatefulWidget {
+  const CreateAnAccountWidget({super.key});
+
+  @override
+  State<CreateAnAccountWidget> createState() => _CreateAnAccountWidgetState();
+}
+
+class _CreateAnAccountWidgetState extends State<CreateAnAccountWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 155.w,
+      height: 18.h,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const IdentificationScreen()),
+                );
+              },
+              child: SizedBox(
+                width: 64.w,
+                height: 18.h,
+                child: Opacity(
+                  opacity: 0.75,
+                  child: FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Text(
+                      'إنشاء حساب',
+                      style: safeGoogleFont(
+                        'Cairo',
+                        color: const Color(0xFF4CAF50),
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w600,
+                        height: 0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: SizedBox(
+              width: 89.w,
+              height: 18.h,
+              child: FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  'ليس لديك حساب؟',
+                  style: safeGoogleFont(
+                    'Cairo',
+                    color: Colors.black,
+                    fontSize: 12.sp,
+                    fontWeight: FontWeight.w600,
+                    height: 0,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

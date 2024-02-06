a11bb482a2a34dc233ff84f67a4f40ec8c02dde5
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-02-06 17:03:16 +0200

    Update forgot password screen UI

--- lib/views/screens/forgot_password_screen.dart
+++ lib/views/screens/forgot_password_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:untitled/views/screens/Identification_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:untitled/views/common_widgets/create_an_account_widget.dart';
 import 'package:untitled/views/screens/login_screen.dart';
+import 'package:untitled/views/utils/app_colors.dart';
+import 'package:untitled/views/utils/app_fonts.dart';
 import '../common_widgets/forgot_password_appbar.dart';
+import '../common_widgets/login_text_field.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
@@ -11,6 +15,8 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final TextEditingController emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +24,7 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0.0,
+        leadingWidth: 31.89.w,
         leading: const ForgotPasswordAppBar(
           screen: LoginScreen(),
         ),
@@ -26,7 +33,92 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
         child: Center(
           child: Column(
             children: [
-              
+              Image.asset(
+                'assets/forgot-password-bro-1.png',
+                width: 299.w,
+                height: 274.h,
+              ),
+              SizedBox(
+                height: 39.38.h,
+              ),
+              FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  'نسيت كلمة المرور',
+                  textAlign: TextAlign.center,
+                  style: safeGoogleFont(
+                    'Cairo',
+                    fontSize: 24.sp,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  'أدخل عنوان البريد الالكتروني',
+                  textAlign: TextAlign.center,
+                  style: safeGoogleFont(
+                    'Cairo',
+                    fontSize: 13.sp,
+                    color: const Color(0xff626262),
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 12.0.h,
+              ),
+              LoginTextField(
+                controller: emailController,
+                lable: 'البريد الالكتروني',
+                keyboardType: TextInputType.emailAddress,
+                hintText: 'example@gmail.com',
+              ),
+              SizedBox(
+                height: 44.0.h,
+              ),
+              MaterialButton(
+                onPressed: () {},
+                child: Container(
+                  width: 263.w,
+                  height: 50.h,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: AppColors.primary),
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0xcc000000),
+                        offset: Offset(0, 0),
+                        blurRadius: 2,
+                      ),
+                    ],
+                  ),
+                  child: SizedBox(
+                    width: 149.w,
+                    height: 20.54.h,
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        'ارسال رمز التحقق',
+                        textAlign: TextAlign.center,
+                        style: safeGoogleFont(
+                          'Cairo',
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.w700,
+                          color: AppColors.primary,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 159.0.h,
+              ),
+              const CreateAnAccountWidget()
             ],
           ),
         ),

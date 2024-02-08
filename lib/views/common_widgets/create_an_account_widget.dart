c8adccb122afd3b0d34dffde0ca3d563cab2889c
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-02-09 01:47:11 +0200

    Fix import statement in create_an_account_widget.dart

--- lib/views/common_widgets/create_an_account_widget.dart
+++ lib/views/common_widgets/create_an_account_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../screens/Identification_screen.dart';
+import '../screens/identification_screen.dart';
 import '../utils/app_fonts.dart';
 
 class CreateAnAccountWidget extends StatefulWidget {

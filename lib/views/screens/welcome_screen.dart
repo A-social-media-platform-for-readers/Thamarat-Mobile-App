22fd2bb05c6de872bad22a55ff119966c89eda9a
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-02-11 16:00:37 +0200

    Refactor welcome screen layout

--- lib/views/screens/welcome_screen.dart
+++ lib/views/screens/welcome_screen.dart
@@ -20,53 +20,51 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     return Scaffold(
         backgroundColor: AppColors.primary,
         body: SafeArea(
-          child: SingleChildScrollView(
-            child: Center(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 26.08.h,
-                  ),
-                  const AppTitle(
-                    textColor: Colors.white,
-                  ),
-                  SizedBox(
-                    height: 15.59.h,
-                  ),
-                  Image.asset(
-                    'assets/reading-list-bro-1.png',
-                    width: 460.w,
-                    height: 460.h,
-                  ),
-                  SizedBox(
-                    height: 37.42.h,
-                  ),
-                  const WelcomeText(),
-                  SizedBox(
-                    height: 29.h,
-                  ),
-                  const WelcomeScreenButton(
-                    lable: 'لدي حساب بالفعل',
-                    textColor: Colors.white,
-                    borderColor: Colors.white,
-                    buttonColor: AppColors.primary,
-                    screen: LoginScreen(),
-                  ),
-                  SizedBox(
-                    height: 17.h,
-                  ),
-                  const WelcomeScreenButton(
-                    lable: 'إنشاء حساب',
-                    textColor: AppColors.color2,
-                    borderColor: Colors.white,
-                    buttonColor: Colors.white,
-                    screen: IdentificationScreen(),
-                  ),
-                  SizedBox(
-                    height: 49.h,
-                  ),
-                ],
-              ),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 26.08.h,
+                ),
+                const AppTitle(
+                  textColor: Colors.white,
+                ),
+                SizedBox(
+                  height: 15.59.h,
+                ),
+                Image.asset(
+                  'assets/reading-list-bro-1.png',
+                  width: 360.w,
+                  height: 338.54.h,
+                ),
+                SizedBox(
+                  height: 37.42.h,
+                ),
+                const WelcomeText(),
+                SizedBox(
+                  height: 29.h,
+                ),
+                const WelcomeScreenButton(
+                  lable: 'لدي حساب بالفعل',
+                  textColor: Colors.white,
+                  borderColor: Colors.white,
+                  buttonColor: AppColors.primary,
+                  screen: LoginScreen(),
+                ),
+                SizedBox(
+                  height: 17.h,
+                ),
+                const WelcomeScreenButton(
+                  lable: 'إنشاء حساب',
+                  textColor: AppColors.color2,
+                  borderColor: Colors.white,
+                  buttonColor: Colors.white,
+                  screen: IdentificationScreen(),
+                ),
+                SizedBox(
+                  height: 49.h,
+                ),
+              ],
             ),
           ),
         ));

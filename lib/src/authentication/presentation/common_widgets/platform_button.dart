2f85a7513b36ebcbc42c110ac85a15b01c8b7ec4
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-03-08 10:45:41 +0200

    Refactor PlatformButton widget to use MaterialButton

--- lib/src/authentication/presentation/common_widgets/platform_button.dart
+++ lib/src/authentication/presentation/common_widgets/platform_button.dart
@@ -1,73 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:untitled/src/core/utils/app_fonts.dart';
 
 class PlatformButton extends StatelessWidget {
   final String lable;
   final Color textColor;
   final String icon;
   final Color buttonColor;
+  final void Function()? onPressed;
 
   const PlatformButton(
       {super.key,
       required this.lable,
       required this.icon,
       required this.buttonColor,
-      required this.textColor});
+      required this.textColor,
+      required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 290.w,
-      height: 52.h,
-      child: Stack(
-        children: [
-          Container(
-            width: 290.w,
-            height: 52.h,
-            decoration: ShapeDecoration(
-              color: buttonColor,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8)),
-              shadows: const [
-                BoxShadow(
-                  color: Color(0x3F000000),
-                  blurRadius: 4,
-                  offset: Offset(0, 4),
-                  spreadRadius: 0,
-                )
-              ],
+    return MaterialButton(
+      onPressed: onPressed,
+      child: SizedBox(
+        width: 290.w,
+        height: 52.h,
+        child: Stack(
+          children: [
+            Container(
+              width: 290.w,
+              height: 52.h,
+              decoration: ShapeDecoration(
+                color: buttonColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                shadows: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.25),
+                    blurRadius: 4,
+                    offset: const Offset(0, 4),
+                    spreadRadius: 0,
+                  )
+                ],
+              ),
             ),
-          ),
-          Positioned(
-            left: 17.w,
-            top: 10.h,
-            child: SizedBox(
-                width: 32.62.w, height: 32.62.h, child: Image.asset(icon)),
-          ),
-          Center(
-            child: Container(
-              width: 55.w,
-              height: 21.h,
-              margin: const EdgeInsetsDirectional.only(
-                  start: 118, end: 117, top: 16, bottom: 15),
-              child: FittedBox(
-                fit: BoxFit
-                    .scaleDown, // Scale the text down to fit the available space
-                child: Text(
-                  lable,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: textColor,
-                    fontSize: 15.sp,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w600,
-                    height: 0,
+            Positioned(
+              left: 17.w,
+              top: 10.h,
+              child: SizedBox(
+                  width: 32.62.w, height: 32.62.h, child: Image.asset(icon)),
+            ),
+            Center(
+              child: Container(
+                width: 55.w,
+                height: 21.h,
+                margin: const EdgeInsetsDirectional.only(
+                    start: 118, end: 117, top: 16, bottom: 15),
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    lable,
+                    textAlign: TextAlign.center,
+                    style: safeGoogleFont(
+                      'Poppins',
+                      color: textColor,
+                      fontSize: 15.sp,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }

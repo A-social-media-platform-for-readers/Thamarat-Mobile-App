3e5b49ef55578106f1e4e490a1482dcc54f22093
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-05-06 02:28:31 +0300

    feat: Add SVG assets for bottom navigation icons and home screen elements

---
+++ lib/src/core/utils/assets.dart
@@ -0,0 +1,12 @@
+class AssetsData {
+  // BottomNavBar Icons
+  static const homeIcon = "assets/home.svg";
+  static const socialMediaIcon = "assets/Chat_Conversation_Circle.svg";
+  static const downloadIcon = "assets/Download_Package.svg";
+  static const profileIcon = "assets/User_Circle.svg";
+  // Home Screen
+  static const homeList = "assets/Hamburger_LG.svg";
+  static const notification = "assets/Bell_Notification.svg";
+  static const advertisement = "assets/Group 12785.png";
+  static const testImage = "assets/Rectangle 29.png";
+}

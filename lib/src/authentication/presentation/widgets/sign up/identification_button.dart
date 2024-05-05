e7b6c1c8d307c346719ced49c18bc84adc1c5a7c
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-05-06 02:28:22 +0300

    chore: Refactor IdentificationButton widget and improve selection functionality

--- lib/src/authentication/presentation/widgets/sign up/identification_button.dart
+++ lib/src/authentication/presentation/widgets/sign up/identification_button.dart
@@ -3,111 +3,74 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:untitled/src/core/utils/app_colors.dart';
 import '../../../../core/utils/app_fonts.dart';
 
-class IdentificationButton extends StatefulWidget {
+class IdentificationButton extends StatelessWidget {
   final String startIdentityIcon;
   final String identityName;
   final String endIdentityIcon;
   final Color newColor;
+  final bool isSelected;
+  final VoidCallback onTap;
 
-  const IdentificationButton(
-      {super.key,
-      required this.startIdentityIcon,
-      required this.endIdentityIcon,
-      required this.newColor,
-      required this.identityName});
-
-  @override
-  State<IdentificationButton> createState() => _IdentificationButtonState();
-}
-
-class _IdentificationButtonState extends State<IdentificationButton> {
-  double widthAnimation = 160.w;
-  Color? colorAnimation = AppColors.lightGray;
-  late String identityIcontAnimation;
-  late String checkboxIcontAnimation = "assets/Icons (2).png";
-  Color? textColorAnimation = AppColors.charcoal;
-
-  @override
-  void initState() {
-    super.initState();
-    identityIcontAnimation = widget.startIdentityIcon;
-  }
+  const IdentificationButton({
+    Key? key,
+    required this.startIdentityIcon,
+    required this.endIdentityIcon,
+    required this.newColor,
+    required this.identityName,
+    required this.isSelected,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: () {
-          setState(() {
-            widthAnimation = widthAnimation == 160.w ? 268.w : 160.w;
-            identityIcontAnimation =
-                identityIcontAnimation == widget.startIdentityIcon
-                    ? widget.endIdentityIcon
-                    : widget.startIdentityIcon;
-            checkboxIcontAnimation =
-                checkboxIcontAnimation == "assets/Icons (2).png"
-                    ? "assets/Icons (1).png"
-                    : "assets/Icons (2).png";
-            colorAnimation = colorAnimation == AppColors.lightGray
-                ? widget.newColor
-                : AppColors.lightGray;
-            textColorAnimation = textColorAnimation == AppColors.charcoal
-                ? Colors.white
-                : AppColors.charcoal;
-          });
-        },
-        child: AnimatedContainer(
-          duration: const Duration(microseconds: 300),
-          child: Container(
-            width: widthAnimation,
-            height: 47.h,
-            decoration: BoxDecoration(
-              color: colorAnimation,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: SizedBox(
-              width: double.infinity,
-              height: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 18,
-                    height: 15,
-                    child: Image.asset(
-                      identityIcontAnimation,
-                    ),
-                  ),
-                  Center(
-                    child: Container(
-                      width: 38.w,
-                      height: 24.h,
-                      margin: EdgeInsets.symmetric(horizontal: 11.w),
-                      child: FittedBox(
-                        fit: BoxFit.scaleDown,
-                        child: Text(
-                          widget.identityName,
-                          textAlign: TextAlign.center,
-                          style: safeGoogleFont(
-                            'Cairo',
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                            color: textColorAnimation,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 24.w,
-                    height: 24.h,
-                    child: Image.asset(
-                      checkboxIcontAnimation,
-                    ),
-                  ),
-                ],
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        width: isSelected ? 268.w : 160.w,
+        height: 47.h,
+        decoration: BoxDecoration(
+          color: isSelected ? newColor : AppColors.lightGray,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 18,
+              height: 15,
+              child: Image.asset(
+                isSelected ? endIdentityIcon : startIdentityIcon,
               ),
             ),
-          ),
-        ));
+            Container(
+              width: 38.w,
+              height: 24.h,
+              margin: EdgeInsets.symmetric(horizontal: 11.w),
+              child: FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  identityName,
+                  textAlign: TextAlign.center,
+                  style: safeGoogleFont(
+                    'Cairo',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: isSelected ? Colors.white : AppColors.charcoal,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 24.w,
+              height: 24.h,
+              child: Image.asset(
+                isSelected ? "assets/Icons (1).png" : "assets/Icons (2).png",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

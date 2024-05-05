1d5bda5f8f85445b05b50416d24f2554f35ebe66
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-05-06 02:31:11 +0300

    Refactor main.dart and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:untitled/src/core/custom_bottomn_nav_bar.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:untitled/src/authentication/presentation/screens/welcome/welcome_screen.dart';
 
 void main() {
@@ -9,7 +9,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, child) {
-        return MaterialApp(
+        return GetMaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'ثمرات Thamarat',
           theme: ThemeData(
@@ -27,7 +26,7 @@ class MyApp extends StatelessWidget {
           home: child,
         );
       },
-      child: const CustomBottomNavBar(),
+      child: const WelcomeScreen(),
     );
   }
 }

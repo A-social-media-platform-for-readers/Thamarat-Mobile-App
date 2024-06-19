8d65d00777bb6da701343a8fa5475e7782489602
Ahmed Diaa <123115981+AhmedDiaa0212@users.noreply.github.com>
2024-06-19 15:02:07 +0300

    feat: Add registration functionality to register screen
    The code changes add registration functionality to the register screen by introducing the `AuthService` class and implementing the `registerUser` method. This allows users to register with their user data and create a new account.

--- lib/src/authentication/Data/auth_service.dart
+++ lib/src/authentication/Data/auth_service.dart
@@ -1,83 +1,80 @@
-import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:untitled/src/authentication/Data/user_mode.dart';
 
 class AuthService {
   final Dio _dio = Dio();
-  static const String baseUrl = 'https://backend-9s26.onrender.com/';
 
-  Future<UserModel> login(String email, String password) async {
+  Future<bool> registerUser(User user) async {
+    const String url = 'https://backend-9s26.onrender.com/auth/register/';
+
     try {
       final response = await _dio.post(
-        '$baseUrl/auth/login/',
+        url,
+        data: user.toJson(),
+        options: Options(headers: {
+          'Content-Type': 'application/json',
+        }),
+      );
+
+      if (response.statusCode == 201) {
+        print('User registered successfully');
+        return true;
+      } else {
+        print('Failed to register user: ${response.statusMessage}');
+        return false;
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        print('DioException: ${e.response?.data}');
+        print('DioException: ${e.response?.headers}');
+        print('DioException: ${e.response?.requestOptions}');
+      } else {
+        print('DioException: ${e.message}');
+      }
+      return false;
+    } catch (e) {
+      print('Error occurred while registering user: $e');
+      return false;
+    }
+  }
+
+  Future<bool> loginUser(String email, String password) async {
+    const String url = 'https://backend-9s26.onrender.com/auth/login/';
+
+    try {
+      final response = await _dio.post(
+        url,
         data: {
           'email': email,
           'password': password,
         },
-        options: Options(
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        ),
+        options: Options(headers: {
+          'Content-Type': 'application/json',
+        }),
       );
 
       if (response.statusCode == 200) {
-        // Save JWT to shared preferences
         final prefs = await SharedPreferences.getInstance();
-        prefs.setString('jwt', response.data['jwt']);
-        // Parse user data and return
-        return UserModel.fromJson(response.data['user']);
+        await prefs.setString('jwt', response.data['jwt']);
+        print('User logged in successfully');
+        return true;
       } else {
-        throw Exception('Failed to login');
+        print('Failed to login: ${response.statusMessage}');
+        return false;
       }
-    } catch (e) {
-      throw Exception('Failed to login: $e');
-    }
-  }
-
-  Future<void> logout() async {
-    try {
-      final response = await _dio.post(
-        '$baseUrl/auth/logout/',
-        options: Options(
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        ),
-      );
-
-      if (response.statusCode == 200) {
-        // Remove JWT from shared preferences
-        final prefs = await SharedPreferences.getInstance();
-        prefs.remove('jwt');
+    } on DioException catch (e) {
+      if (e.response != null) {
+        print('DioException: ${e.response?.data}');
+        print('DioException: ${e.response?.headers}');
+        print('DioException: ${e.response?.requestOptions}');
       } else {
-        throw Exception('Failed to logout');
+        print('DioException: ${e.message}');
       }
+      return false;
     } catch (e) {
-      throw Exception('Failed to logout: $e');
-    }
-  }
-
-  Future<UserModel> register(Map<String, dynamic> userData) async {
-    try {
-      final response = await _dio.post(
-        '$baseUrl/auth/register/',
-        data: jsonEncode(userData),
-        options: Options(
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        ),
-      );
-
-      if (response.statusCode == 201) {
-        return UserModel.fromJson(response.data);
-      } else {
-        throw Exception('Failed to register');
-      }
-    } catch (e) {
-      throw Exception('Failed to register: $e');
+      print('Error occurred while logging in: $e');
+      return false;
     }
   }
 }
--- lib/src/authentication/Data/user_mode.dart
+++ lib/src/authentication/Data/user_mode.dart
@@ -1,31 +1,43 @@
-class UserModel {
+class User {
   final String identity;
   final String name;
   final String email;
   final String password;
+  final String? birthDate;
+  final String? gender;
+  final String? profileImage;
+  final String? bio;
+  final String? phone;
+  final int? address;
 
-  UserModel({
+  User({
     required this.identity,
     required this.name,
     required this.email,
     required this.password,
+    this.birthDate,
+    this.gender,
+    this.profileImage,
+    this.bio,
+    this.phone,
+    this.address,
   });
 
-  factory UserModel.fromJson(Map<String, dynamic> json) {
-    return UserModel(
-      identity: json['identity'],
-      name: json['name'],
-      email: json['email'],
-      password: json['password'],
-    );
-  }
-
   Map<String, dynamic> toJson() {
-    return {
-      'identity': identity,
-      'name': name,
-      'email': email,
-      'password': password,
+    final Map<String, dynamic> data = {
+      "identity": identity,
+      "name": name,
+      "email": email,
+      "password": password,
     };
+
+    if (profileImage != null) data["profile_image"] = profileImage;
+    if (bio != null) data["bio"] = bio;
+    if (birthDate != null) data["birth_date"] = birthDate;
+    if (gender != null) data["gender"] = gender;
+    if (phone != null) data["phone"] = phone;
+    if (address != null) data["address"] = address;
+
+    return data;
   }
 }
